a3b1cdb700b7a198893cb9e8127524837ace6929
hashmati <hashmati@student.chalmers.se>
2020-09-15 13:25:22 +0200

    Added files for yarn, needle, and note
    Added controller and model files for entities yarn, needle, and note.

---
+++ server/models/needle.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var needleSchema = new Schema({
+	size: {type: String},
+	material_id: {type: Schema.Types.ObjectId, ref ‘Material’}
+});
+
+module.exports = mongoose.model('notes', noteSchema);
