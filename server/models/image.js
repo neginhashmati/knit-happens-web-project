723eb41c8dd6b9071a428c8dfe533ad4bc124e36
Shonaigh Douglas <shonaigh@student.chalmers.se>
2020-09-15 13:11:10 +0200

    Added image and material schemas and endpoints
    Added image and material schemas to the model folder and images and materials endpoint files to the controllers folder.

---
+++ server/models/image.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// Set schema for Images
+var imageSchema = new Schema({
+	user_id: {type: Schema.Types.ObjectId, ref ‘User’},
+	img: {
+        data: Buffer,
+        contentType: String
+        }
+});
+
+module.exports = mongoose.model('images', imageSchema);
