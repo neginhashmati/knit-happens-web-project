e7feebcd8612a77ff231c430059fadc93c5b5424
Shonaigh Douglas <shonaigh@student.chalmers.se>
2020-09-16 15:41:57 +0200

    Update image and materials controllers
    Edits and/or corrections have been made to the image and materials controllers

--- server/models/image.js
+++ server/models/image.js
@@ -4,10 +4,8 @@ var Schema = mongoose.Schema;
 
 // Set schema for Images
 var imageSchema = new Schema({
-	img: {
-        data: Buffer,
-        contentType: String
-        }
+	path: String,
+        url: String
 });
 
 module.exports = mongoose.model('images', imageSchema);
