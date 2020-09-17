76884cd56c0f5d926db7be5584d5199538260cc2
Shonaigh Douglas <shonaigh@student.chalmers.se>
2020-09-17 21:26:48 +0200

    Delete materials
    Deleted materials model and controllers and added its attributes to yarn and needle models

--- server/models/needle.js
+++ server/models/needle.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var needleSchema = new Schema({
-	size: {type: String}
+	name: {type: String},
+	source_name: {type: String},
+	cost: {type: Number},
+	owned: {type: Boolean},
+	size: {type: String},
+    note: {type: String},
+    projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }]
 });
 
 module.exports = mongoose.model('needles', needleSchema);
--- server/models/yarn.js
+++ server/models/yarn.js
@@ -2,10 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var yarnSchema = new Schema({
+    name: {type: String},
+	source_name: {type: String},
+	cost: {type: Number},
+	owned: {type: Boolean},
     brand: {type: String},
     color: {type: String},
     weight: {type: String},
-    fiber: {type: String}
+    fiber: {type: String},
+    note: {type: String},  
+    projects: [{ type: Schema.Types.ObjectId, ref: 'Project' }]
 });    
 
 module.exports = mongoose.model('yarns', yarnSchema); 
