b4d75c66f337c79a73498715c134708ad9a5bd46
hashmati <hashmati@student.chalmers.se>
2020-09-27 12:35:43 +0200

    Added code for Google authorization.

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import GAuth from 'vue-google-oauth2'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,6 +10,13 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+
+const gauthOption = {
+  clientId: '417993214770-pmp56g18el151s2nbvkuuqv2jsl6qq7h.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GAuth, gauthOption)
 
 new Vue({
   router,
