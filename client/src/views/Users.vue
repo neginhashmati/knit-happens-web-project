8bb43199da44329332a76c98e60389c2e3a9d8c8
hashmati <hashmati@student.chalmers.se>
2020-10-03 14:47:52 +0200

    Changed all variables to be for a user

--- client/src/views/Users.vue
+++ client/src/views/Users.vue
@@ -6,16 +6,16 @@
           <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
         </b-col> -->
         <b-col cols="15">
-          <b-button v-on:click="createProject">Create New Project</b-button>
+          <b-button v-on:click="createUser">Create New Project</b-button>
         </b-col>
         <b-col cols="4">
-          <b-button v-on:click="deleteAllProjects">Delete All Projects</b-button>
+          <b-button v-on:click="deleteAllUsers">Delete All Projects</b-button>
         </b-col>
       </b-row>
       <h1>My projects:</h1>
       <b-row align-h="center">
-        <b-col cols="12" sm="6" md="4" v-for="project in projects" v-bind:key="project._id">
-            <project-item v-bind:project="project" v-on:del-project="deleteProject"/>
+        <b-col cols="12" sm="6" md="4" v-for="user in users" v-bind:key="user._id">
+            <project-item v-bind:project="users" v-on:del-project="deleteUser"/>
         </b-col>
       </b-row>
     </b-container>
@@ -32,16 +32,16 @@ export default {
   },
   mounted() {
     console.log('PAGE is loaded')
-    // Load the real projects from the server
-    Api.get('/projects')
+    // Load the real users from the server
+    Api.get('/users')
       .then(response => {
         console.log(response.data)
-        this.projects = response.data.projects
+        this.users = response.data.users
       })
       .catch(error => {
         this.message = error.message
         console.error(error)
-        this.projects = []
+        this.users = []
         // TODO: display error message
       })
       .then(() => {
@@ -50,28 +50,28 @@ export default {
   },
   data() {
     return {
-      projects: [],
+      users: [],
       message: '',
       text: ''
     }
   },
   methods: {
-    deleteProject(id) {
-      Api.delete(`/projects/${id}`)
+    deleteUser(id) {
+      Api.delete(`/users/${id}`)
         .catch(error => {
           console.error(error)
         })
     },
-    createProject() {
+    createUser() {
       console.log(this.text)
-      Api.post('/projects')
+      Api.post('/users')
         .catch(error => {
           console.error(error)
         })
     },
-    deleteAllProjects() {
+    deleteAllUsers() {
       console.log(this.text)
-      Api.delete('/projects')
+      Api.delete('/users')
         .catch(error => {
           console.error(error)
         })
